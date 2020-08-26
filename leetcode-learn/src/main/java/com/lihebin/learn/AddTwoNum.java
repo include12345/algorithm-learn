62f9d88e38f637bd145e981b41d32728a0435643
lihebin <lihebin@shouqianba.com>
2020-08-26 22:40:03 +0800

    两数相加

---
+++ leetcode-learn/src/main/java/com/lihebin/learn/AddTwoNum.java
@@ -0,0 +1,54 @@
+package com.lihebin.learn;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by lihebin on 2020/8/26.
+ *
+ * 两数相加
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+ 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+ 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+ 示例：
+
+ 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ 输出：7 -> 0 -> 8
+ 原因：342 + 465 = 807
+
+ *
+ */
+public class AddTwoNum {
+
+    private static List<Integer> addTwoNumbers(List<Integer> list1, List<Integer> list2) {
+        List<Integer> resultList = new LinkedList<>();
+        int carry = 0;
+        if (list1.size() > 0 && list2.size() > 0) {
+            int size;
+            if (list1.size() >= list2.size()) {
+                size = list1.size();
+            } else {
+                size = list2.size();
+            }
+            for (int i = 0; i < size; i++) {
+                if (list1.get(i) != null && list2.get(i) != null) {
+                    carry = carry + list1.get(i) + list2.get(i);
+                } else if (list1.get(i) != null){
+                    carry = carry + list1.get(i);
+                } else {
+                    carry = carry + list2.get(i);
+                }
+                int sum = carry;
+                carry = carry / 10;
+                sum = sum % 10;
+                resultList.add(sum);
+            }
+        }
+        return resultList;
+
+    }
+}
